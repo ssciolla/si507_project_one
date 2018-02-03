8362804368dc6624945a237cd9f78fcf0a787016
ssciolla <samgsciolla@gmail.com>
2018-02-02 23:47:48 -0500

    Completed Task Two and changed test file structure

--- proj1_w18.py
+++ proj1_w18.py
@@ -1,10 +1,19 @@
+import json
 
 class Media:
 
-	def __init__(self, title="No Title", author="No Author", release_year="No Release Year"):
-		self.title = title
-		self.author = author
-		self.release_year = release_year
+	def __init__(self, title="No Title", author="No Author", release_year="No Release Year", json=None):
+		if json == None:
+			self.title = title
+			self.author = author
+			self.release_year = release_year
+		else:
+			if "trackName" in json.keys():
+				self.title = json["trackName"]
+			else:
+				self.title = json["collectionName"]
+			self.author = json["artistName"]
+			self.release_year = json["releaseDate"][:4]
 
 	def __str__(self):
 		string_rep = "{} by {} ({})".format(self.title, self.author, self.release_year)
@@ -15,11 +24,16 @@ class Media:
 
 class Song(Media):
 
-	def __init__(self, title="No Title", author="No Author", release_year="No Release Year", album="No Album name", track_length="No Track Length", genre="No Genre"):
-		super().__init__(title, author, release_year)
-		self.album = album
-		self.track_length = track_length
-		self.genre = genre # Assignment doesn't say to do this, but otherwise, I'd have to input it again in the __str__ method.
+	def __init__(self, title="No Title", author="No Author", release_year="No Release Year", album="No Album name", track_length="No Track Length", genre="No Genre", json=None):
+		super().__init__(title, author, release_year, json)
+		if json == None:
+			self.album = album
+			self.track_length = track_length
+			self.genre = genre # Assignment doesn't say to do this, but otherwise, I'd have to input it again in the __str__ method.
+		else:
+			self.album = json["collectionName"]
+			self.track_length = json["trackTimeMillis"]
+			self.genre = json["primaryGenreName"]
 
 	def __str__(self):
 		base_rep = super().__str__()
@@ -30,15 +44,19 @@ class Song(Media):
 		if self.track_length == "No Track Length":
 			return self.track_length
 		else:
-			time_in_sec = (self.track_length / 1000)
+			time_in_sec = self.track_length / 1000
 			return time_in_sec
 
 class Movie(Media):
 
-	def __init__(self, title="No Title", author="No Author", release_year="No Release Year", rating="No Rating", movie_length="No Movie Length"):
-		super().__init__(title, author, release_year)
-		self.rating = rating
-		self.movie_length = movie_length
+	def __init__(self, title="No Title", author="No Author", release_year="No Release Year", rating="No Rating", movie_length="No Movie Length", json=None):
+		super().__init__(title, author, release_year, json)
+		if json == None:
+			self.rating = rating
+			self.movie_length = movie_length
+		else:
+			self.rating = json["contentAdvisoryRating"]
+			self.movie_length = json["trackTimeMillis"]
 
 	def __str__(self):
 		base_rep = super().__str__()
--- proj1_w18_test.py
+++ proj1_w18_test.py
@@ -1,63 +1,85 @@
 import unittest
+import json
 import proj1_w18 as proj1
+
+def create_test_cases():
+    media_one = proj1.Media()
+    media_two = proj1.Media("1999", "Prince", "1982")
+    song_one = proj1.Song()
+    song_two = proj1.Song("6 Inch", "Beyonce", "2016", "Lemonade", 260000, "R&B")
+    movie_one = proj1.Movie()
+    movie_two = proj1.Movie("Pan's Labyrinth", "Guillermo del Toro", "2006", "R", 7105000)
+    test_cases_part_one = [media_one, media_two, song_one, song_two, movie_one, movie_two]
+    return test_cases_part_one
+
+def fetch_sample_json():
+    file_open = open("sample_json.json", "r")
+    list = json.loads(file_open.read())
+    file_open.close()
+    return list
+
+def check_results(list):
+    for item in list:
+        if item != "All Good":
+            return False
+    return True
+
+def test_if_correct_vars(class_instance):
+    if isinstance(class_instance, proj1.Song):
+        results = []
+        results.append(getattr(class_instance, "movie_length", "All Good"))
+        results.append(getattr(class_instance, "rating", "All Good"))
+        return check_results(results)
+    elif isinstance(class_instance, proj1.Movie):
+        results = []
+        results.append(getattr(class_instance, "track_length", "All Good"))
+        results.append(getattr(class_instance, "album", "All Good"))
+        results.append(getattr(class_instance, "genre", "All Good"))
+        return check_results(results)
+    elif isinstance(class_instance, proj1.Media):
+        results = []
+        results.append(getattr(class_instance, "track_length", "All Good"))
+        results.append(getattr(class_instance, "album", "All Good"))
+        results.append(getattr(class_instance, "genre", "All Good"))
+        results.append(getattr(class_instance, "movie_length", "All Good"))
+        results.append(getattr(class_instance, "rating", "All Good"))
+        return check_results(results)
 
 class TestMedia(unittest.TestCase):
 
-    # media_one = proj1.Media()
-    # media_two = proj1.Media("1999", "Prince", "1982")
-    #
-    # song_one = proj1.Song()
-    # song_two = proj1.Song("6 Inch", "Beyonce", "2016", "Lemonade", 260000, "R&B")
-    #
-    # movie_one = proj1.Movie()
-    # movie_two = proj1.Movie("Pan's Labyrinth", "Guillermo del Toro", "2006", "R", 7105000)
-
     def testConstructor(self):
+        # Testing Media instances
+        self.assertEqual(create_test_cases()[0].title, "No Title")
+        self.assertEqual(create_test_cases()[0].author, "No Author")
+        self.assertEqual(create_test_cases()[0].release_year, "No Release Year")
+        self.assertEqual(create_test_cases()[1].title, "1999")
+        self.assertEqual(create_test_cases()[1].author, "Prince")
+        self.assertEqual(create_test_cases()[1].release_year, "1982")
+        self.assertTrue(test_if_correct_vars(create_test_cases()[1]))
 
-        media_one = proj1.Media()
-        media_two = proj1.Media("1999", "Prince", "1982")
+        # Testing Song instances
+        self.assertEqual(create_test_cases()[2].album, "No Album name")
+        self.assertEqual(create_test_cases()[2].track_length, "No Track Length")
+        self.assertEqual(create_test_cases()[3].album, "Lemonade")
+        self.assertEqual(create_test_cases()[3].track_length, 260000)
+        self.assertTrue(test_if_correct_vars(create_test_cases()[3]))
 
-        self.assertEqual(media_one.title, "No Title")
-        self.assertEqual(media_one.author, "No Author")
-        self.assertEqual(media_one.release_year, "No Release Year")
-        self.assertEqual(media_two.title, "1999")
-        self.assertEqual(media_two.author, "Prince")
-        self.assertEqual(media_two.release_year, "1982")
-
-        song_one = proj1.Song()
-        song_two = proj1.Song("6 Inch", "Beyonce", "2016", "Lemonade", 260000, "R&B")
-
-        self.assertEqual(song_one.album, "No Album name")
-        self.assertEqual(song_one.track_length, "No Track Length")
-        self.assertEqual(song_two.album, "Lemonade")
-        self.assertEqual(song_two.track_length, 260000)
-
-        movie_one = proj1.Movie()
-        movie_two = proj1.Movie("Pan's Labyrinth", "Guillermo del Toro", "2006", "R", 7115000)
-
-        self.assertEqual(movie_one.rating, "No Rating")
-        self.assertEqual(movie_one.movie_length, "No Movie Length")
-        self.assertEqual(movie_two.rating, "R")
-        self.assertEqual(movie_two.movie_length, 7115000)
+        # Testing Movie instances
+        self.assertEqual(create_test_cases()[4].rating, "No Rating")
+        self.assertEqual(create_test_cases()[4].movie_length, "No Movie Length")
+        self.assertEqual(create_test_cases()[5].rating, "R")
+        self.assertEqual(create_test_cases()[5].movie_length, 7105000)
+        self.assertTrue(test_if_correct_vars(create_test_cases()[5]))
 
     def testStringMethods(self):
-
-        media_one = proj1.Media()
-        media_two = proj1.Media("1999", "Prince", "1982")
-        song_one = proj1.Song()
-        song_two = proj1.Song("6 Inch", "Beyonce", "2016", "Lemonade", 260000, "R&B")
-        movie_one = proj1.Movie()
-        movie_two = proj1.Movie("Pan's Labyrinth", "Guillermo del Toro", "2006", "R", 7115000)
-
-        self.assertEqual(media_one.__str__(), "No Title by No Author (No Release Year)")
-        self.assertEqual(media_two.__str__(), "1999 by Prince (1982)")
-        self.assertEqual(song_one.__str__(), "No Title by No Author (No Release Year) [No Genre]")
-        self.assertEqual(song_two.__str__(), "6 Inch by Beyonce (2016) [R&B]")
-        self.assertEqual(movie_one.__str__(), "No Title by No Author (No Release Year) [No Rating]")
-        self.assertEqual(movie_two.__str__(), "Pan's Labyrinth by Guillermo del Toro (2006) [R]")
+        self.assertEqual(create_test_cases()[0].__str__(), "No Title by No Author (No Release Year)")
+        self.assertEqual(create_test_cases()[1].__str__(), "1999 by Prince (1982)")
+        self.assertEqual(create_test_cases()[2].__str__(), "No Title by No Author (No Release Year) [No Genre]")
+        self.assertEqual(create_test_cases()[3].__str__(), "6 Inch by Beyonce (2016) [R&B]")
+        self.assertEqual(create_test_cases()[4].__str__(), "No Title by No Author (No Release Year) [No Rating]")
+        self.assertEqual(create_test_cases()[5].__str__(), "Pan's Labyrinth by Guillermo del Toro (2006) [R]")
 
     def testLenMethods(self):
-
         media_one = proj1.Media()
         media_two = proj1.Media("1999", "Prince", "1982")
         song_one = proj1.Song()
@@ -71,5 +93,37 @@ class TestMedia(unittest.TestCase):
     #    self.assertEqual(len(song_two), 260)
         self.assertEqual(movie_one.__len__(), "No Movie Length")
         self.assertEqual(movie_two.__len__(), 119)
+
+    def testMediaJSON(self):
+        bridget_jones = proj1.Media(json=fetch_sample_json()[2])
+        print(bridget_jones)
+        self.assertEqual(bridget_jones.title, "Bridget Jones's Diary (Unabridged)")
+        self.assertEqual(bridget_jones.author, "Helen Fielding")
+        self.assertEqual(bridget_jones.release_year, "2012")
+        self.assertEqual(bridget_jones.__str__(), "Bridget Jones's Diary (Unabridged) by Helen Fielding (2012)")
+        self.assertEqual(bridget_jones.__len__(), 0)
+
+    def testTrackJSON(self):
+        hey_jude = proj1.Song(json=fetch_sample_json()[1])
+        print(hey_jude)
+        self.assertEqual(hey_jude.title, "Hey Jude")
+        self.assertEqual(hey_jude.author, "The Beatles")
+        self.assertEqual(hey_jude.release_year, "1968")
+        self.assertEqual(hey_jude.album, "TheBeatles 1967-1970 (The Blue Album)")
+        self.assertEqual(hey_jude.track_length, 431333)
+        self.assertEqual(hey_jude.genre, "Rock")
+        self.assertEqual(hey_jude.__str__(), "Hey Jude by The Beatles (1968) [Rock]")
+        self.assertEqual(hey_jude.__len__(), 431.333)
+
+    def testMovieJSON(self):
+        jaws = proj1.Movie(json=fetch_sample_json()[0])
+        print(jaws) # Why is this getting printed second?
+        self.assertEqual(jaws.title, "Jaws")
+        self.assertEqual(jaws.author, "Steven Spielberg")
+        self.assertEqual(jaws.release_year, "1975")
+        self.assertEqual(jaws.rating, "PG")
+        self.assertEqual(jaws.movie_length, 7451455)
+        self.assertEqual(jaws.__str__(), "Jaws by Steven Spielberg (1975) [PG]")
+        self.assertEqual(jaws.__len__(), 124)
 
 unittest.main()
